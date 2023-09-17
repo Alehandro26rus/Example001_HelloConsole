3688887a39b75375d59302d831503dd8de2bb96f
Alehandro_26rus <skash96@yandex.ru>
2023-09-17 14:45:58 +0300

    Example006_Base

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,7 +1,8 @@
-﻿Console.Write("Введите имя пользователя: ");
+﻿System.Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-if (username.ToLower() == "masha")
+if (username.ToLower() == "Маша")
 {
     Console.WriteLine("Ура, это же МАША!");
 }
